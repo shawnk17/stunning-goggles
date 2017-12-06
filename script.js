864a7d24f3e49b92f7a727c0de77f68bb5d8081b
Dan Hoffman <dghoffman@gmail.com>
2017-12-06 14:07:02 -0500

    added javascript for challenge 1

---
+++ script.js
@@ -0,0 +1,27 @@
+var btnPlay = document.getElementById("btn-play"),
+    output = document.getElementById("output"),
+    num = 1;
+
+btnPlay.addEventListener("click", function(){
+    setInterval(counter, 500);
+});
+
+function counter(){
+    if(num <= 100){
+        if(num % 3 == 0 && num % 5 == 0){
+            output.innerHTML = "<h1 class='ex'>FIZZBUZZ EXTREME!</h1>";
+        } else {
+            if(num % 3 == 0){
+                output.innerHTML = "<h3>FIZZ!</h3>";
+            } else if (num % 5 == 0) {
+                output.innerHTML = "<h3>BUZZ!</h3>";
+            } else {
+                output.innerHTML = "<p>" + num + "</p>";
+            }
+        }
+    } else {
+        clearInterval();
+        output.innerHTML = "<p>GAME OVER</p>";
+    }
+    num++;
+}
