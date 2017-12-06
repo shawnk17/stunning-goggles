c24971d752c6e5536d5ce13dbf0b19e47a0d2212
shawn <axe2781@gmail.com>
2017-12-06 14:05:01 -0500

    carss

--- cars.js
+++ cars.js
@@ -1,10 +1,10 @@
 var makeInput = document.getElementById("make"),
     modelInput = document.getElementById("model"),
-    yearInput = document.getElementById("year");
+    yearInput = document.getElementById("year"),
     vinInput = document.getElementById("vin"),
     addButton = document.getElementById("addButton"),
     displayList = document.getElementById("display-list"),
-    car = [];
+    cars = [];
 
 var Car = function Car(make, model, year, vin) {
     this.make = make;
@@ -12,19 +12,19 @@ var Car = function Car(make, model, year, vin) {
     this.year = year;
     this.vin = vin;
     this.getDisplayText = function () {
-        return this.make + " by " + this.model + "by " + this.year + "by " + this.vin;
-    }
-}
+        return this.make + " by " + this.model + " by " + this.year + " by " + this.vin;
+    };
+};
 
 var renderList = function renderList() {
     displayList.innerHTML = "";
-    books.forEach(function (car, index, arr) {
-        displayList.innerHTML += car.getDisplayText() + "<br>";
+    cars.forEach(curCar => {
+        displayList.innerHTML += curCar.getDisplayText() + "<br>";
     });
 };
 
 addButton.addEventListener("click", function () {
-    var newCar = new Car(makeInput.value, modelInput.value);
-    Car.push(newCar);
+    var newCar = new Car(makeInput.value, modelInput.value, yearInput.value, vinInput.value);
+    cars.push(newCar);
     renderList();
 });
